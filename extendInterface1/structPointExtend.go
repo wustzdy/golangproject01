0112cffeea0188b09b854436397a2f597ae31570
zhudayang <zhudayang@sensetime.com>
2022-11-27 14:44:39 +0800

    接口版多台

---
+++ extendInterface1/structPointExtend.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+type Usber interface {
+	start()
+	stop()
+}
+
+// 电脑
+type Computer struct {
+}
+
+func (c Computer) work(u Usber) {
+	u.start()
+	u.stop()
+
+}
+
+// 手机
+type Phone struct {
+	name string
+}
+
+func (p Phone) start() {
+	fmt.Println(p.name, "开机")
+
+}
+func (p Phone) stop() {
+	fmt.Println(p.name, "关机")
+
+}
+
+// 照相机
+type Camera struct {
+}
+
+func (c Camera) start() {
+	fmt.Println("照相启动")
+
+}
+func (c Camera) stop() {
+	fmt.Println("照相关机")
+
+}
+
+func main() {
+	var computer = Computer{}
+	var phone = Phone{
+		name: "小米",
+	}
+	computer.work(phone)
+
+	var camera = Camera{}
+	computer.work(camera)
+
+}
