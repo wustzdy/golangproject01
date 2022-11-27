99476fa553c36b81d91e39195fb5e195cbc6d5cb
zhudayang <zhudayang@sensetime.com>
2022-11-27 17:18:33 +0800

    add gorm.Model

--- mysql/test/ormTets.go
+++ mysql/test/ormTets.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"time"
 )
 
 // UserInfo 用户信息
@@ -14,6 +16,20 @@ type UserInfo struct {
 	Hobby  string
 }
 
+// 定义模型
+type Student struct {
+	gorm.Model
+	Name         string
+	Age          sql.NullInt64
+	Birthday     *time.Time
+	Email        string  `gorm:"type:varchar(100);unique_index"`
+	Role         string  `gorm:"size:255"`        // 设置字段大小为255
+	MemberNumber *string `gorm:"unique;not null"` // 设置会员号（member number）唯一并且不为空
+	Num          int     `gorm:"AUTO_INCREMENT"`  // 设置 num 为自增类型
+	Address      string  `gorm:"index:addr"`      // 给address字段创建名为addr的索引
+	IgnoreMe     int     `gorm:"-"`               // 忽略本字段
+}
+
 func main() {
 	dsn := "root:123456@tcp(127.0.0.1:3306)/test_gorm?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -21,8 +37,12 @@ func main() {
 		panic(err)
 		fmt.Println(err)
 	}
-	db.AutoMigrate(&UserInfo{})
 
+	//createUserInfo(db)
+	createStudent(db)
+}
+
+func createUserInfo(db *gorm.DB) {
 	// 自动迁移
 	db.AutoMigrate(&UserInfo{})
 
@@ -44,4 +64,8 @@ func main() {
 	db.Model(&u).Update("hobby", "双色球")
 	// 删除
 	db.Delete(&u)
+}
+
+func createStudent(db *gorm.DB) {
+	db.AutoMigrate(&Student{})
 }
