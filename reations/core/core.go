4501d50bc9a408a1a80bf0207420bfc5e0e6d3d0
zhudayang <zhudayang@sensetime.com>
2022-12-04 16:02:29 +0800

    add reations

---
+++ reations/core/core.go
@@ -0,0 +1,23 @@
+package core
+
+import (
+	"fmt"
+	"golangproject01/reations/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+var err error
+
+func Init() {
+	dsn := "root:123456@tcp(127.0.0.1:3306)/test_gorm1?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	DB = db
+	DB.AutoMigrate(&models.GirlGod{}) //这个时候不糊自动创建Dog//需要
+
+	DB.AutoMigrate(&models.GirlGod{}, &models.Dog{})
+}
---
+++ reations/test/test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"golangproject01/reations/core"
+	"golangproject01/reations/models"
+)
+
+func main() {
+	initCore()
+	//oneToOne()
+	selectOne()
+}
+
+func initCore() {
+	core.Init()
+}
+
+func hasAs() {
+
+	d := models.Dog{
+		Name: "张三",
+	}
+	g := models.GirlGod{
+		Name: "美女",
+		Dog:  d,
+	}
+	core.DB.Create(&g)
+}
+
+func selectOne() {
+	var girl models.GirlGod
+	core.DB.First(&girl, 1)
+	fmt.Println("girl:", girl)
+
+	//这个时候 只会查询girl相关的，但是dog查不出来， 需要改造
+	var girl1 models.GirlGod
+	core.DB.Preload("Dog").First(&girl1, 1)
+	fmt.Println("girl:", girl1)
+
+}
