062996e6ecbc0f85004c753471bddf7a98f582d6
zhudayang <zhudayang@sensetime.com>
2022-12-04 15:30:29 +0800

    add Preload

--- hasAs/test/test.go
+++ hasAs/test/test.go
@@ -1,20 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"golangproject01/hasAs/core"
 	"golangproject01/hasAs/models"
 )
 
 func main() {
 	initCore()
-	oneToOne()
+	//oneToOne()
+	selectOne()
 }
 
 func initCore() {
 	core.Init()
 }
 
-func oneToOne() {
+func hasAs() {
 
 	d := models.Dog{
 		Name: "张三",
@@ -24,4 +26,16 @@ func oneToOne() {
 		Dog:  d,
 	}
 	core.DB.Create(&g)
+}
+
+func selectOne() {
+	var girl models.GirlGod
+	core.DB.First(&girl, 1)
+	fmt.Println("girl:", girl)
+
+	//这个时候 只会查询girl相关的，但是dog查不出来， 需要改造
+	var girl1 models.GirlGod
+	core.DB.Preload("Dog").First(&girl1, 1)
+	fmt.Println("girl:", girl1)
+
 }
