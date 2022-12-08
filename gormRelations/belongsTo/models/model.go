f44a7fa2a19cfc2f5a1ab18d8e4269e3d3cdfb50
zhudayang <zhudayang@sensetime.com>
2022-12-08 16:39:39 +0800

    update belongTo

--- gormRelations/belongsTo/models/model.go
+++ gormRelations/belongsTo/models/model.go
@@ -3,12 +3,12 @@ package models
 import "gorm.io/gorm"
 
 // `User` 属于 `Company`，`CompanyID` 是外键
+// user 是属于 company 的，就是每个 user 有且只能对应分配给一个 company。
 type User struct {
 	gorm.Model
-	Name         string
-	CompanyRefer int
-	Company      Company `gorm:"foreignKey:CompanyRefer"`
-	//// 使用 CompanyRefer 作为外键
+	Name      string
+	CompanyID int
+	Company   Company
 }
 
 type Company struct {
--- gormRelations/belongsTo/test/test.go
+++ gormRelations/belongsTo/test/test.go
@@ -1,11 +1,25 @@
 package main
 
-import "golangproject01/gormRelations/belongsTo/core"
+import (
+	"golangproject01/gormRelations/belongsTo/core"
+	"golangproject01/gormRelations/belongsTo/models"
+)
 
 func main() {
 	Init()
+	createTest()
 }
 
 func Init() {
 	core.Init()
+}
+
+func createTest() {
+	user := models.User{
+		Name: "张三",
+		Company: models.Company{
+			Name: "上海科技",
+		},
+	}
+	core.DB.Create(&user)
 }
