73b459c9eb8f13e3d34d80160ea3d5a70161f413
zhudayang <zhudayang@sensetime.com>
2022-12-08 16:41:55 +0800

    update hasAs

--- gormRelations/hasTo/models/model.go
+++ gormRelations/hasTo/models/model.go
@@ -5,6 +5,8 @@ import (
 )
 
 // User 有一张 CreditCard，UserID 是外键
+// 1，user 是拥有 creditcard 的，creditcard 有且只能被一个 user 拥有。
+// 2，Creditcard是属于user的，或者说user关联Creditcard
 type User struct {
 	gorm.Model
 	CreditCard CreditCard
