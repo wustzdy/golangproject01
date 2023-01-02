3f073623dd6ff797439875ccb1cb437336d7847a
zhudayang <zhudayang@sensetime.com>
2023-01-02 21:07:03 +0800

    golang中指针的操作

---
+++ arrayAndSlice/pointer/pointer.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 10
+	fmt.Printf("a的值:%v a的类型:%T a的地址:%p\n", a, a, &a) //a的值:10 a的类型:int a的地址:0xc00001a098
+
+	//指针也是一个变量，但是他是一个特殊的变量，它存储的数据不是一个普通的值，而是另外一个变量的内存地址
+	var b = 10
+	var p = &b                           //p指针变量 p的类型 *int
+	fmt.Printf("p的值:%v p的类型:%T\n", p, p) //p的值:0xc000124018 p的类型:*int
+
+	pointerTest()
+}
+
+func pointerTest() {
+	//golang 里面的变量都有一个内存地址
+	var a = 10
+	var b = &a                                       //b指针变量 b的类型 *int
+	fmt.Printf("a的值:%v a的类型:%T a的地址:%p\n", a, a, &a) //a的值:10 a的类型:int a的地址:0xc000124030
+	fmt.Printf("b的值:%v b的类型:%T b的地址:%p\n", b, b, &b) //b的值:0xc000124030 b的类型:*int b的地址:0xc00011c020
+
+	//可以看出p的值就是a的内存地址，但是b也有自己的内存地址
+}
