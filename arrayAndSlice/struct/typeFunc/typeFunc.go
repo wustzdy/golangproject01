540512cc6dde0c731691fcc3e465beca02f7591f
zhudayang <zhudayang@sensetime.com>
2023-01-02 22:44:44 +0800

    自定义类型添加方法

--- arrayAndSlice/struct/typeFunc/typeFunc.go
+++ arrayAndSlice/struct/typeFunc/typeFunc.go
@@ -1,6 +1,16 @@
 package main
 
+import "fmt"
+
 // 自定义类型添加方法
+type MyInt int
+
+func (m MyInt) printInfo() {
+	fmt.Println("我是自定义类型里面的自定义方法")
+
+}
 func main() {
+	var a MyInt = 20
+	a.printInfo()
 
 }
