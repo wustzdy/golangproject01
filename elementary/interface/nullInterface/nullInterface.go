162e8688e14b55d0511870a74288fabe7670787a
zhudayang <zhudayang@sensetime.com>
2023-01-03 22:53:40 +0800

    空接口类型

---
+++ elementary/interface/nullInterface/nullInterface.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type A interface{} //空接口，表示没有约束，任意类型的接口都可以实现空接口
+
+func main() {
+	nullInterfaceTest()
+	nullFunc()
+}
+
+func nullInterfaceTest() {
+	var a A
+	var str = "你好golang"
+	a = str                           //让字符串实现A这个接口
+	fmt.Printf("值:%v  类型:%T\n", a, a) //值:你好golang  类型:string
+
+	var num = 20
+	a = num                           //表示让int类型实现A这个接口
+	fmt.Printf("值:%v  类型:%T\n", a, a) //值:20  类型:int
+
+	var flag = true
+	a = flag
+	fmt.Printf("值:%v  类型:%T\n", a, a) //值:true  类型:bool
+}
+
+func nullFunc() {
+	//golang中空接口也可以直接当做类型来使用，可以表示任意类型
+	var a interface{}
+	a = 20
+	fmt.Printf("值:%v  类型:%T\n", a, a) ///值:20  类型:int
+	a = "你好golang"
+	fmt.Printf("值:%v  类型:%T\n", a, a) //值:你好golang  类型:string
+	a = true
+	fmt.Printf("值:%v  类型:%T\n", a, a) //值:true  类型:bool
+
+}
